f89a537e58d36cbe64e427714bb2e9edcf0f0393
jenningsm42 <jenningsm42@gmail.com>
2017-01-20 21:16:50 -0800

    Add screen stats

---
+++ core/src/com/roblox/ggj/App.java
@@ -0,0 +1,24 @@
+package com.roblox.ggj;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class App extends com.badlogic.gdx.Game {
+	public static final float scaleRatio = 8.f;
+	
+	@Override
+	public void create () {
+		setScreen(new SplashScreen(this));
+	}
+
+	public static Sprite createScaledSprite(Texture texture) {
+		Sprite sprite = new Sprite(texture);
+		sprite.setSize(sprite.getWidth() * scaleRatio,
+				sprite.getHeight() * scaleRatio);
+		return sprite;
+	}
+}
---
+++ core/src/com/roblox/ggj/SplashScreen.java
@@ -0,0 +1,79 @@
+package com.roblox.ggj;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+/**
+ * Created by KaseiFox on 1/20/17.
+ */
+
+public class SplashScreen implements Screen {
+    private App app;
+    private Stage stage;
+
+    private class SplashActor extends Actor {
+        private Sprite splash;
+
+        public SplashActor() {
+            splash = App.createScaledSprite(new Texture("splash.png"));
+            splash.setPosition((Gdx.graphics.getWidth() - splash.getWidth()) / 2.f - 200.f, (Gdx.graphics.getHeight() - splash.getHeight()) / 2.f);
+        }
+
+        @Override
+        public void draw(Batch batch, float parentAlpha) {
+            splash.draw(batch);
+        }
+    }
+
+    public SplashScreen(App app) {
+        this.app = app;
+    }
+
+    @Override
+    public void show() {
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+
+        stage.addActor(new SplashActor());
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.act(delta);
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
