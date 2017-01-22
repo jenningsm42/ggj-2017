cbaf58d56dac3b2369aa270f2e8b2a36a3d52e34
BenjaminLang <benjaminlang12@gmail.com>
2017-01-21 16:51:23 -0800

    added shooting button:

--- core/src/com/roblox/ggj/ShootButtonActor.java
+++ core/src/com/roblox/ggj/ShootButtonActor.java
@@ -14,20 +14,21 @@ public class ShootButtonActor extends Actor {
     private Sprite sprite;
     private Texture buttonTexture;
     private ProjectileFactory factory;
+    private TrumpActor trump;
 
-
-    public ShootButtonActor(ProjectileFactory factory){
+    public ShootButtonActor(ProjectileFactory factory, TrumpActor trump){
         buttonTexture = new Texture("button.png");
         sprite = App.createScaledSprite(buttonTexture);
         sprite.setX(Gdx.graphics.getWidth() - sprite.getWidth());
         sprite.setY(sprite.getY());
         this.factory = factory;
+        this.trump = trump;
     }
 
     @Override
     public void act(float delta) {
         if(Gdx.input.isTouched()){
-            factory.createProjectile();
+            factory.createProjectile(null, trump, ProjectileType.MONEY);
         }
     }
 
